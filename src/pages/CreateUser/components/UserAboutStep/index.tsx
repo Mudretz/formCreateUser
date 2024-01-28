cc407dd8308fc1e023a538b1420ec179e45e6dd1
Gungaev Zhamso <zham.drag@gmail.com>
2024-01-28 14:04:26 +0800

    исправления

--- src/pages/CreateUser/components/UserAboutStep/index.tsx
+++ src/pages/CreateUser/components/UserAboutStep/index.tsx
@@ -19,8 +19,7 @@ import { getUser } from "@src/entities/user/slice/user.selectors";
 import { transformUserDataForApi } from "@src/entities/user/helpers";
 import { TypeModals } from "@src/shared/types/common";
 import { TYPE_MODALS } from "@src/shared/constants/common";
-import { ModalSuccess } from "@src/shared/components/common/ModalSuccess";
-import { ModalError } from "@src/shared/components/common/ModalError";
+import { ModalError, ModalSuccess } from "@src/shared/components/common";
 import styles from "./styles.module.scss";
 
 export const UserAboutStep: FC = () => {
